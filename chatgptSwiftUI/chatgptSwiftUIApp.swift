644d5f2e2fd8ca9ede39e5754f17eae7aa2b85d9
pestov29 <ipestov2001@gmail.com>
2023-04-20 15:52:00 +0300

    Initial Commit

---
+++ chatgptSwiftUI/chatgptSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatgptSwiftUIApp.swift
+//  chatgptSwiftUI
+//
+//  Created by Ivan Pestov on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct chatgptSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
